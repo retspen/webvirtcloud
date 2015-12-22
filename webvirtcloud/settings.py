decd5ab4a65a74ff8d6504ef770070b519597f8c
Jan Krcmar <honza801@gmail.com>
2015-12-22 09:48:54 +0100

    add remoteuser auth middleware classes

--- webvirtcloud/settings.py
+++ webvirtcloud/settings.py
@@ -38,9 +38,14 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.RemoteUserMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.RemoteUserBackend',
 )
 
 ROOT_URLCONF = 'webvirtcloud.urls'
