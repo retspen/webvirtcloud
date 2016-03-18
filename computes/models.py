fc56e66555ca7c365381ad2a39ec83df6f45a39f
andrem <andreoandre@gmail.com>
2016-03-18 12:07:42 -0300

    add migration for new column details for hypervisor.

--- computes/models.py
+++ computes/models.py
@@ -6,6 +6,7 @@ class Compute(models.Model):
     hostname = models.CharField(max_length=20)
     login = models.CharField(max_length=20)
     password = models.CharField(max_length=14, blank=True, null=True)
+    details = models.CharField(max_length=50, null=True, blank=True) 
     type = models.IntegerField()
 
     def __unicode__(self):
