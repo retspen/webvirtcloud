f959dc7016778af4266e2031030ada0b046ad5fe
Ing. Jan KRCMAR <honza801@civ.zcu.cz>
2017-06-06 12:49:33 +0200

    settings: TEMPLATE_DIRS is deprecated, using TEMPLATES instead

--- webvirtcloud/settings.py
+++ webvirtcloud/settings.py
@@ -78,9 +78,13 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,  'templates'),
-)
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [ os.path.join(BASE_DIR, 'templates'), ],
+        'APP_DIRS': True,
+    }
+]
 
 ## WebVirtCloud settings
 
