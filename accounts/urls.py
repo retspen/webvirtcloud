3628e5f355b6060a5759e6206a175862faab971e
Bandic007 <r.v.mirchev@gmail.com>
2018-07-06 15:48:38 +0300

    fixing django version to 1.11.14 + other

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,11 +4,8 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    url(r'^login/$', 'django.contrib.auth.views.login',
-        {'template_name': 'login.html'}, name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout',
-        {'template_name': 'logout.html'}, name='logout'),
-    url(r'^profile/$', views.profile, name='profile'),
-    url(r'^$', views.accounts, name='accounts'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    url(r'^profile/$', views.profile, name='profile'), url(r'^$', views.accounts, name='accounts'),
     url(r'^profile/(?P<user_id>[0-9]+)/$', views.account, name='account'),
 ]
