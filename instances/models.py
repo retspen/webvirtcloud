2585e64cfd75de49e9dfbf8a545140d90b6d0cc6
catborise <catborise@gmail.com>
2018-07-25 09:33:06 +0300

    While cloning volume it breaks if volume name is longer than 20 char. It i more realistic longer than 20 char.

--- instances/models.py
+++ instances/models.py
@@ -4,7 +4,7 @@ from computes.models import Compute
 
 class Instance(models.Model):
     compute = models.ForeignKey(Compute)
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=120)
     uuid = models.CharField(max_length=36)
     is_template = models.BooleanField(default=False)
     created = models.DateField(auto_now_add=True)
