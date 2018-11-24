9a8935d052ff2626b74a9d3aef5826ef1919898b
Dmitriy Kalinin <dmitry.kalinin.email@gmail.com>
2018-11-25 00:56:17 +0200

    added users list

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', UserView.as_view(), name="user_view"),
+    path('list', UsersView.as_view(), name="users_view"),
 ]
--- backend/user/views.py
+++ backend/user/views.py
@@ -2,8 +2,14 @@ from __future__ import unicode_literals
 
 from django.contrib.auth.models import User
 
-from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.generics import RetrieveUpdateAPIView, ListAPIView
 from .serializers import UserSerializer
+
+
+class UsersView(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
 
 class UserView(RetrieveUpdateAPIView):
     serializer_class = UserSerializer
