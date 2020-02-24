fc8612c60478849a80323e55e6c0586769f7646d
catborise <catborise@yahoo.com>
2020-02-24 10:08:48 +0300

    change os.environ set style which is new one.

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webvirtcloud.settings")
+    os.environ["DJANGO_SETTINGS_MODULE"] = "webvirtcloud.settings"
 
     from django.core.management import execute_from_command_line
 
