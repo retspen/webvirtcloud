5afa84ee19853e71d70e431b38ece7c3f1bd23ba
Real-Gecko <alexandrbezenkov@gmail.com>
2020-05-27 18:46:37 +0600

    Added change password permission Replaces SHOW_PROFILE_EDIT_PASSWORD option Can be set on per user or per group basis

--- accounts/migrations/0002_auto_20200602_1004.py
+++ accounts/migrations/0002_auto_20200602_1004.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0002_permissionset'),
+        ('accounts', '0001_initial'),
     ]
 
     operations = [
--- accounts/migrations/0003_permissionset.py
+++ accounts/migrations/0003_permissionset.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0001_initial'),
+        ('accounts', '0002_auto_20200602_1004'),
     ]
 
     operations = [
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
             ],
             options={
-                'permissions': (('change_password', 'Can change password'), ),
+                'permissions': (('change_password', 'Can change password'),),
                 'managed': False,
                 'default_permissions': (),
             },
