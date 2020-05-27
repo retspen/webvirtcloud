e48f331125f552b6a004f0542a3e33ba13efecae
Real-Gecko <alexandrbezenkov@gmail.com>
2020-05-27 18:41:34 +0600

    Fix admin urls not included into main urls.py

--- webvirtcloud/urls.py
+++ webvirtcloud/urls.py
@@ -2,16 +2,14 @@ from django.urls import include, path
 
 from instances.views import index
 from console.views import console
-# from django.contrib import admin
 
 urlpatterns = [
     path('', index, name='index'),
-
+    path('admin/', include(('admin.urls', 'admin'), namespace='admin')),
     path('instances/', include('instances.urls')),
     path('accounts/', include('accounts.urls')),
     path('computes/', include('computes.urls')),
     path('logs/', include('logs.urls')),
     path('datasource/', include('datasource.urls')),
     path('console/', console, name='console'),
-    # url(r'^admin/', include(admin.site.urls)),
 ]
