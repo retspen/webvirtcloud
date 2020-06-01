91a6dcc9f60ec5b77f6cd15f82310fcced6a1930
catborise <catborise@yahoo.com>
2020-06-01 16:08:04 +0300

    Move Allow Empty Password Appsettings to settings.py file

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,8 +9,9 @@ class UserAddForm(forms.Form):
     name = forms.CharField(label="Name",
                            error_messages={'required': _('No User name has been entered')},
                            max_length=20)
-    password = forms.CharField(required=not settings.ALLOW_EMPTY_PASSWORD, error_messages={'required': _('No password has been entered')},)
-
+    password = forms.CharField(required=not settings.ALLOW_EMPTY_PASSWORD,
+                               error_messages={'required': _('No password has been entered')},)
+   
     def clean_name(self):
         name = self.cleaned_data['name']
         have_symbol = re.match('^[a-z0-9]+$', name)
