d9c23291debdc3c683eb18d247c09c7ce149ea77
catborise <catborise@yahoo.com>
2021-06-10 15:17:53 +0300

    rearrange search functionality

--- static/js/filter-table.js
+++ static/js/filter-table.js
@@ -4,12 +4,12 @@ function filter_table() {
     $('.searchable tr').filter(function () {
         return rex.test($(this).text());
     }).show();
-    Cookies.set("instances_filter", $(this).val(), { expires: 1 });
+    Cookies.set(document.title + "_filter", $(this).val(), { expires: 1 });
 }
 $(document).ready(function () {
-    instances_filter_cookie = Cookies.get("instances_filter");
-    if (instances_filter_cookie) {
-        $('#filter').val(instances_filter_cookie);
+    filter_cookie = Cookies.get(document.title + "_filter");
+    if (filter_cookie) {
+        $('#filter').val(filter_cookie);
         $('#filter').each(filter_table);
     }
     (function ($) {
